271a04d3e856d8be4440d23eb907e9712c73c13c
soon9086 <soonjongnim@naver.com>
2024-03-09 13:11:02 +0900

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: ['/anyone-can-visit-this-route'],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ['/no-auth-in-this-route'],
+  // clerk 홈페이지에서 내 webhooks탭에서 내 Endpoint URL에 설정한 값
+  publicRoutes: ['/api/webhooks/clerk']
 });
  
 export const config = {
